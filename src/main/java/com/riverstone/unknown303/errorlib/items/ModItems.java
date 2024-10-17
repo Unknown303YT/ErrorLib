9908a0a76fcc861c8b2cc7010c7483fde1ac32b5
Unknown_303YT <unknown.303yt@gmail.com>
2024-10-17 16:20:36 +1030

    Start looking at creating ArmorTrimRegistry
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

--- src/main/java/com/riverstone/unknown303/errorlib/items/ModItems.java
+++ src/main/java/com/riverstone/unknown303/errorlib/items/ModItems.java
@@ -2,7 +2,11 @@ package com.riverstone.unknown303.errorlib.items;
 
 import com.riverstone.unknown303.errorlib.ErrorMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.SmithingTemplateItem;
+import net.minecraft.world.item.armortrim.TrimPattern;
+import net.minecraft.world.item.armortrim.TrimPatterns;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,6 +19,9 @@ public class ModItems {
 //    public static final RegistryObject<Item> CORRUPTED_HEART = ITEMS.register("corrupted_heart",
 //            () -> new Item(new Item.Properties().rarity(Rarity.UNCOMMON)
 //                    .stacksTo(1).fireResistant()));
+
+//    TrimPattern;
+//    TrimPatterns
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
